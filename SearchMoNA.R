d81e3a902b84be3a919f3bd606896b3c81ca097c
Michael Witting <Michael Witting>
2016-11-06 14:02:31 +0100

    first test function searchByFormula

--- SearchMoNA.R
+++ SearchMoNA.R
@@ -3,4 +3,29 @@ library(httr)
 
 MONASEARCH <- "http://mona.fiehnlab.ucdavis.edu/rest/spectra/search" 
 JSONCONTENT <- '"Content-Type" = "application/json"' 
+MSPCONTENT <- '"Accept: text/msp'
+
+#search by molecular formula
+searchByFormula <- function(formula, returnType = "json") {
+  
+  #GET request
+  if(returnType == "json") {
+    
+    #return as json
+    req <- GET(MONASEARCH,
+               add_headers(JSONCONTENT),
+               query = list(query = paste0('compound.metaData=q=\'name=="molecular formula" and value=="', formula, '"\'')))
+    
+  } else if(returnType == "msp") {
+    
+    #return as msp
+    req <- GET(MONASEARCH,
+               add_headers(MSPCONTENT),
+               query = list(query = paste0('compound.metaData=q=\'name=="molecular formula" and value=="', formula, '"\'')))
+    
+  }
+  
+  return(content(req))
+  
+}
 
